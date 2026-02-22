43b747a0548a07cbfa4aff24e40fd11920ba862e
dakshyadavdtu <146624239+dakshyadavdtu@users.noreply.github.com>
2026-02-22 18:39:25 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -34,8 +34,8 @@ export const signUp=async (req,res)=>{
         let token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
           })
           
@@ -66,8 +66,8 @@ export const login=async(req,res)=>{
         let token =await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
           })
           
@@ -107,8 +107,8 @@ export const googleSignup = async (req,res) => {
         let token =await genToken(user._id)
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "Lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
           })
           
@@ -186,4 +186,5 @@ export const resetPassword = async (req,res) => {
     } catch (error) {
         return res.status(500).json({message:`Reset Password error ${error}`})
     }
+
 }
